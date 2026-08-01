2aefbf1139b28104b70b67b9089857ee441a396d
shubhambhagat52 <shubham253035@iiitmanipur.ac.in>
2026-08-01 10:03:01 +0530

    leetcode82solution

---
+++ LinkedList/leetcode82.java
@@ -0,0 +1,44 @@
+
+// Problem: 82. Remove Duplicates from Sorted List II
+// Platform: LeetCode
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode curr) {
+        ListNode dummy = new ListNode(Integer.MIN_VALUE);
+        ListNode tail = dummy;
+        while(curr!=null){
+            if(curr.next!=null && curr.val == curr.next.val){
+                int temp= curr.val;
+                while(curr!=null && curr.val== temp){
+                    curr=curr.next;
+                }
+            }
+            else{
+
+            
+
+            
+            tail.next= curr;
+            tail= curr;
+            curr= curr.next;
+        }
+        }
+        tail.next= null;
+        return dummy.next;
+
+        
+    }
+}
